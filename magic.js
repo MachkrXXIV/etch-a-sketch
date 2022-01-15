1859837570af04cc4a8d0598d512bd5830d95b0a
Tin <bachmach24k@gmail.com>
2022-01-14 20:00:52 -0600

    Finished minimum viable product

--- magic.js
+++ magic.js
@@ -1,9 +1,13 @@
-const DEFAULT_COLOR = "33333"
+const DEFAULT_COLOR = "0, 0%, 1%";
+const DEFAULT_MODE = "marker";
 const DEFAULT_SIZE = 16;
 
 
 const body = document.querySelector("body");
 const gridCont = document.querySelector(".grid-container");
+const slider = document.querySelector(".slider");
+const clearBtn = document.querySelector("#clear");
+const area = document.querySelector(".area");
 
 
 function makeGrid(size) {
@@ -14,21 +18,34 @@ function makeGrid(size) {
     gridCont.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridCont.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   }
-  let gridCells = gridCont.querySelectorAll("div");
+  let gridCells = gridCont.querySelectorAll("div"); //creates selector and event listeners for all cells
   gridCells.forEach(cell => {
     cell.addEventListener("mouseover", (e) => {
-      e.target.style.backgroundColor = "black";
+      paint(e);
     });
   })
 }
 
 function paint(cell) {
-  cell.style.backgroundColor = "black";
+  cell.target.style.backgroundColor = `hsl(${DEFAULT_COLOR})`;
 }
 
 function resetCanvas() {
-  allCells.classList.remove("painted");
+  let gridCells = gridCont.querySelectorAll("div");
+  gridCells.forEach(cell => cell.style.backgroundColor = "hsl(0, 100%, 100%)");
 }
+
+function changeArea() {
+  let gridCells = gridCont.querySelectorAll("div");
+  gridCells.forEach(cell => cell.remove());
+  area.textContent = `${slider.value} x ${slider.value}`;
+  makeGrid(slider.value);
+}
+
+// Event listeners
+clearBtn.addEventListener("click", () => resetCanvas());
+slider.addEventListener("input", () => changeArea());
+
 
 // Main
 makeGrid(DEFAULT_SIZE);
